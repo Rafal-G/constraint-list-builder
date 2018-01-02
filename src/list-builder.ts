9893bf54e59bcf35aa8b58eb06d03b87071b84ee
Rafal Gostynski <rgostynski@gmail.com>
2018-01-02 13:10:07 +1300

    added tsconfig and list-builder file

---
+++ src/list-builder.ts
@@ -0,0 +1,15 @@
+export namespace ListBuilder {
+    
+    export function registerNodes() {
+        console.log('registering nodes');
+    }
+    
+    export function addNode() {
+        console.log('addingNode');
+    }
+
+    export function getList() {
+        console.log('getting list');
+    }
+
+  }
